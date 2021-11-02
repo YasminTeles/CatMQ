a403d57c727a6b8ac2745b6020620dee0a859538
Yasmin Teles <teles.yasmin@gmail.com>
2021-11-02 18:47:12 -0300

    feat: add message formatter

---
+++ message/message.go
@@ -0,0 +1,91 @@
+package message
+
+import (
+	"encoding/json"
+	"log"
+)
+
+const (
+	OperationPut     = "PUT"
+	OperationGet     = "GET"
+	OperationEmpty   = "EMP"
+	OperationError   = "ERR"
+	OperationOK      = "OK"
+	OperationMessage = "MSG"
+
+	MessageEmpty = ""
+	MessageError = "Operation failed!"
+)
+
+type Message struct {
+	Operation string `json:"operation"`
+	Data      string `json:"data"`
+}
+
+func NewMessage(data string) *Message {
+	msg := &Message{}
+	msg.ToUnpack(data)
+
+	return msg
+}
+
+func NewResponseMessage(data string) *Message {
+	return &Message{
+		Operation: OperationMessage,
+		Data:      data,
+	}
+}
+
+func NewOKMessage() *Message {
+	return &Message{
+		Operation: OperationOK,
+		Data:      MessageEmpty,
+	}
+}
+
+func NewErrorMessage() *Message {
+	return &Message{
+		Operation: OperationError,
+		Data:      MessageError,
+	}
+}
+
+func NewEmptyMessage() *Message {
+	return &Message{
+		Operation: OperationEmpty,
+		Data:      MessageEmpty,
+	}
+}
+
+func NewPutMessage(data string) *Message {
+	return &Message{
+		Operation: OperationPut,
+		Data:      data,
+	}
+}
+
+func NewGetMessage() *Message {
+	return &Message{
+		Operation: OperationGet,
+		Data:      MessageEmpty,
+	}
+}
+
+func (message *Message) ToUnpack(data string) {
+	if err := json.Unmarshal([]byte(data), message); err != nil {
+		log.Panicf("Some unpack error: %s.\n", err)
+	}
+}
+
+func (message *Message) ToPack() string {
+	data, err := json.Marshal(message)
+	if err != nil {
+		log.Panicf("Some pack error: %s.\n", err)
+	}
+
+	return string(data)
+}
+
+func (message *Message) IsEmptyData() bool {
+	return !(len(message.Data) > 0)
+}
---
+++ message/message_test.go
@@ -0,0 +1,141 @@
+package message
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMessage(t *testing.T) {
+	t.Parallel()
+
+	data := `{"operation":"PUT","data":"<some data>"}`
+	message := NewMessage(data)
+
+	expected := &Message{
+		Operation: OperationPut,
+		Data:      "<some data>",
+	}
+	assert.Exactly(t, expected, message)
+}
+
+func TestResponseNewMessage(t *testing.T) {
+	t.Parallel()
+
+	data := "Message received."
+	message := NewResponseMessage(data)
+
+	responseMessage := &Message{
+		Operation: OperationMessage,
+		Data:      data,
+	}
+	assert.Exactly(t, responseMessage, message)
+}
+
+func TestNewOKMessage(t *testing.T) {
+	t.Parallel()
+
+	message := NewOKMessage()
+
+	okMessage := &Message{
+		Operation: OperationOK,
+		Data:      MessageEmpty,
+	}
+
+	assert.Exactly(t, okMessage, message)
+}
+
+func TestErrorMessage(t *testing.T) {
+	t.Parallel()
+
+	message := NewErrorMessage()
+
+	errorMessage := &Message{
+		Operation: OperationError,
+		Data:      MessageError,
+	}
+
+	assert.Exactly(t, errorMessage, message)
+}
+
+func TestEmptyMessage(t *testing.T) {
+	t.Parallel()
+
+	message := NewEmptyMessage()
+
+	emptyMessage := &Message{
+		Operation: OperationEmpty,
+		Data:      MessageEmpty,
+	}
+
+	assert.Exactly(t, emptyMessage, message)
+}
+
+func TestPutMessage(t *testing.T) {
+	t.Parallel()
+
+	data := "Message sending"
+	message := NewPutMessage(data)
+
+	putMessage := &Message{
+		Operation: OperationPut,
+		Data:      data,
+	}
+
+	assert.Exactly(t, putMessage, message)
+}
+
+func TestGetMessage(t *testing.T) {
+	t.Parallel()
+
+	message := NewGetMessage()
+
+	getMessage := &Message{
+		Operation: OperationGet,
+		Data:      MessageEmpty,
+	}
+
+	assert.Exactly(t, getMessage, message)
+}
+
+func TestToUnpack(t *testing.T) {
+	t.Parallel()
+
+	message := &Message{}
+	msg := `{"operation":"PUT","data":"Ja chegou o disco voador..."}`
+
+	message.ToUnpack(msg)
+
+	expected := &Message{
+		"PUT",
+		"Ja chegou o disco voador...",
+	}
+	assert.Exactly(t, expected, message)
+}
+
+func TestToPack(t *testing.T) {
+	t.Parallel()
+
+	message := &Message{
+		"GET",
+		"Ja se foi o disco voador...",
+	}
+
+	msg := message.ToPack()
+
+	expected := `{"operation":"GET","data":"Ja se foi o disco voador..."}`
+	assert.Exactly(t, expected, msg)
+}
+
+func TestIsEmptyData(t *testing.T) {
+	t.Parallel()
+
+	message := &Message{
+		Operation: "GET",
+		Data:      "",
+	}
+
+	isEmptyData := message.IsEmptyData()
+
+	assert.True(t, isEmptyData)
+}
